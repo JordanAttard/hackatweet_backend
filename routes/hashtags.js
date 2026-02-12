4ef8fcd66c8e81c5ffe79ab02adb718d175e2715
Florian DCL <docruzeiro.f@gmail.com>
2026-02-12 13:32:12 +0000

    ajout de la route hashtags + déclaration de la route dans app.js

---
+++ routes/hashtags.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var router = express.Router();
+
+require("../models/connection");
+const Hashtag = require("../models/hashtags");
+
+router.get("/trends", (req, res) => {
+  Hashtag.find().then((data) => {
+    if (data.length > 0) {
+      const trends = data.map((tag) => {
+        return {
+          tagName: tag.tagName,
+          count: tag.tweets.length,
+        };
+      });
+
+      //trends.sort((a, b) => b.count - a.count); //je ne sais pas si on en a besoin
+
+      res.json({ result: true, hashtags: trends });
+    } else {
+      res.json({ result: true, hashtags: [] });
+    }
+  });
+});
+
+module.exports = router;
