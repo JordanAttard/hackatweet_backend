64230ae8f0c22ccbcefa8aa3bc2f97b46bfc0ca1
Florian DCL <docruzeiro.f@gmail.com>
2026-02-12 14:01:24 +0000

    ajout de routes hashtags get

--- routes/hashtags.js
+++ routes/hashtags.js
@@ -14,13 +14,25 @@ router.get("/trends", (req, res) => {
         };
       });
 
-      //trends.sort((a, b) => b.count - a.count); //je ne sais pas si on en a besoin
+      trends.sort((a, b) => b.count - a.count); //je ne sais pas si on en a besoin
 
       res.json({ result: true, hashtags: trends });
     } else {
       res.json({ result: true, hashtags: [] });
     }
   });
+});
+
+router.get("/:tagName", (req, res) => {
+  Hashtag.findOne({ tagName: { $regex: new RegExp(req.params.tagName, "i") } })
+    .populate("tweets")
+    .then((data) => {
+      if (data) {
+        res.json({ result: true, tweets: data.tweets });
+      } else {
+        res.json({ result: false, error: "Hashtag not found" });
+      }
+    });
 });
 
 module.exports = router;
