90d05ea67c53610f790221b64e10c1bfdf92762c
Florian DCL <docruzeiro.f@gmail.com>
2026-02-12 11:09:41 +0000

    ajout de la route post signin

--- routes/users.js
+++ routes/users.js
@@ -33,4 +33,24 @@ router.post("/signup", (req, res) => {
   });
 });
 
+router.post("/signin", (req, res) => {
+  if (!checkBody(req.body, ["username", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+
+  User.findOne({ username: req.body.username }).then((data) => {
+    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({
+        result: true,
+        token: data.token,
+        firstname: data.firstname,
+        username: data.username,
+      });
+    } else {
+      res.json({ result: false, error: "User not found or wrong password" });
+    }
+  });
+});
+
 module.exports = router;
