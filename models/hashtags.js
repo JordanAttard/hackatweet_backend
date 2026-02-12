71040d88ceb230afb1a76e066ad6999da860c5df
Florian DCL <docruzeiro.f@gmail.com>
2026-02-12 10:23:31 +0000

    push dans le models hashtags

--- models/hashtags.js
+++ models/hashtags.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const hashtagSchema = mongoose.Schema({
   tagName: { type: String, required: true, unique: true },
-  tweets: [{ type: mongoose.Schema.Types.ObjectId, ref: "tweets" }], // Lien des tweets
+  tweets: [{ type: mongoose.Schema.Types.ObjectId, ref: "tweets" }], // Lien vers les tweets
 });
 
 const Hashtag = mongoose.model("hashtags", hashtagSchema);
