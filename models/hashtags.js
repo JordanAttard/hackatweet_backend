546021ade33af4132d7f2368a516f3bc59f782f8
Florian DCL <docruzeiro.f@gmail.com>
2026-02-12 10:04:47 +0000

    ajout de models hashtags

---
+++ models/hashtags.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const hashtagSchema = mongoose.Schema({
+  tagName: { type: String, required: true, unique: true },
+  tweets: [{ type: mongoose.Schema.Types.ObjectId, ref: "tweets" }], // Lien des tweets
+});
+
+const Hashtag = mongoose.model("hashtags", hashtagSchema);
+module.exports = Hashtag;
